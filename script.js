af892fd2eeb6b0b51598104a99add61b7551c9dc
chickenONsteak <austinchenwd@gmail.com>
2025-07-18 19:48:51 +0800

    class

--- script.js
+++ script.js
@@ -1,127 +1,69 @@
-// ASSETS
-const clubsA =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/English_pattern_ace_of_clubs.svg/40px-English_pattern_ace_of_clubs.svg.png";
-const clubs2 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/English_pattern_2_of_clubs.svg/40px-English_pattern_2_of_clubs.svg.png";
-const clubs3 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/English_pattern_3_of_clubs.svg/40px-English_pattern_3_of_clubs.svg.png";
-const clubs4 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/English_pattern_4_of_clubs.svg/40px-English_pattern_4_of_clubs.svg.png";
-const clubs5 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/English_pattern_5_of_clubs.svg/40px-English_pattern_5_of_clubs.svg.png";
-const clubs6 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/English_pattern_6_of_clubs.svg/40px-English_pattern_6_of_clubs.svg.png";
-const clubs7 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/English_pattern_7_of_clubs.svg/40px-English_pattern_7_of_clubs.svg.png";
-const clubs8 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/English_pattern_8_of_clubs.svg/40px-English_pattern_8_of_clubs.svg.png";
-const clubs9 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/English_pattern_9_of_clubs.svg/40px-English_pattern_9_of_clubs.svg.png";
-const clubs10 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/English_pattern_10_of_clubs.svg/40px-English_pattern_10_of_clubs.svg.png";
-const clubsJ =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/English_pattern_jack_of_clubs.svg/40px-English_pattern_jack_of_clubs.svg.png";
-const clubsQ =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/English_pattern_queen_of_clubs.svg/40px-English_pattern_queen_of_clubs.svg.png";
-const clubsK =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/English_pattern_king_of_clubs.svg/40px-English_pattern_king_of_clubs.svg.png";
-const diamondsA =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/English_pattern_ace_of_diamonds.svg/40px-English_pattern_ace_of_diamonds.svg.png";
-const diamonds2 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/English_pattern_2_of_diamonds.svg/40px-English_pattern_2_of_diamonds.svg.png";
-const diamonds3 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/English_pattern_3_of_diamonds.svg/40px-English_pattern_3_of_diamonds.svg.png";
-const diamonds4 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/English_pattern_4_of_diamonds.svg/40px-English_pattern_4_of_diamonds.svg.png";
-const diamonds5 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/English_pattern_5_of_diamonds.svg/40px-English_pattern_5_of_diamonds.svg.png";
-const diamonds6 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/English_pattern_6_of_diamonds.svg/40px-English_pattern_6_of_diamonds.svg.png";
-const diamonds7 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/English_pattern_7_of_diamonds.svg/40px-English_pattern_7_of_diamonds.svg.png";
-const diamonds8 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/English_pattern_8_of_diamonds.svg/40px-English_pattern_8_of_diamonds.svg.png";
-const diamonds9 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/English_pattern_9_of_diamonds.svg/40px-English_pattern_9_of_diamonds.svg.png";
-const diamonds10 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/English_pattern_10_of_diamonds.svg/40px-English_pattern_10_of_diamonds.svg.png";
-const diamondsJ =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/English_pattern_jack_of_diamonds.svg/40px-English_pattern_jack_of_diamonds.svg.png";
-const diamondsQ =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/English_pattern_queen_of_diamonds.svg/40px-English_pattern_queen_of_diamonds.svg.png";
-const diamondsK =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/English_pattern_king_of_diamonds.svg/40px-English_pattern_king_of_diamonds.svg.png";
-const heartsA =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/English_pattern_ace_of_hearts.svg/40px-English_pattern_ace_of_hearts.svg.png";
-const hearts2 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/English_pattern_2_of_hearts.svg/40px-English_pattern_2_of_hearts.svg.png";
-const hearts3 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/English_pattern_3_of_hearts.svg/40px-English_pattern_3_of_hearts.svg.png";
-const hearts4 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/English_pattern_4_of_hearts.svg/40px-English_pattern_4_of_hearts.svg.png";
-const hearts5 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/English_pattern_5_of_hearts.svg/40px-English_pattern_5_of_hearts.svg.png";
-const hearts6 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/English_pattern_6_of_hearts.svg/40px-English_pattern_6_of_hearts.svg.png";
-const hearts7 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/English_pattern_7_of_hearts.svg/40px-English_pattern_7_of_hearts.svg.png";
-const hearts8 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/English_pattern_8_of_hearts.svg/40px-English_pattern_8_of_hearts.svg.png";
-const hearts9 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/English_pattern_9_of_hearts.svg/40px-English_pattern_9_of_hearts.svg.png";
-const hearts10 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/English_pattern_10_of_hearts.svg/40px-English_pattern_10_of_hearts.svg.png";
-const heartsJ =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/English_pattern_jack_of_hearts.svg/40px-English_pattern_jack_of_hearts.svg.png";
-const heartsQ =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/English_pattern_queen_of_hearts.svg/40px-English_pattern_queen_of_hearts.svg.png";
-const heartsK =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/English_pattern_king_of_hearts.svg/40px-English_pattern_king_of_hearts.svg.png";
-const spadesA =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/English_pattern_ace_of_spades.svg/40px-English_pattern_ace_of_spades.svg.png";
-const spades2 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/English_pattern_2_of_spades.svg/40px-English_pattern_2_of_spades.svg.png";
-const spades3 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/English_pattern_3_of_spades.svg/40px-English_pattern_3_of_spades.svg.png";
-const spades4 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/English_pattern_4_of_spades.svg/40px-English_pattern_4_of_spades.svg.png";
-const spades5 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/English_pattern_5_of_spades.svg/40px-English_pattern_5_of_spades.svg.png";
-const spades6 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/English_pattern_6_of_spades.svg/40px-English_pattern_6_of_spades.svg.png";
-const spades7 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/English_pattern_7_of_spades.svg/40px-English_pattern_7_of_spades.svg.png";
-const spades8 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/English_pattern_8_of_spades.svg/40px-English_pattern_8_of_spades.svg.png";
-const spades9 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/English_pattern_9_of_spades.svg/40px-English_pattern_9_of_spades.svg.png";
-const spades10 =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/English_pattern_10_of_spades.svg/40px-English_pattern_10_of_spades.svg.png";
-const spadesJ =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/English_pattern_jack_of_spades.svg/40px-English_pattern_jack_of_spades.svg.png";
-const spadesQ =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/English_pattern_queen_of_spades.svg/40px-English_pattern_queen_of_spades.svg.png";
-const spadesK =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/English_pattern_king_of_spades.svg/40px-English_pattern_king_of_spades.svg.png";
-
+/*--------------------------------- Classes ---------------------------------*/
 class Cards {
-  constructor(
-    value = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
-    suit = ["clubs", "diamonds", "hearts", "spades"],
-    img
-  ) {
-    this.value = value;
+  constructor(rank, suit) {
+    this.rank = rank;
     this.suit = suit;
-    this.img = img;
+  }
+  // method to convert the non-int values to int
+  returnValue() {
+    if (this.rank === "J" || this.rank === "Q" || this.rank === "K") {
+      return 10;
+    } else if (this.rank === "A") {
+      return 11;
+    }
+  }
+}
+
+class Deck extends Cards {
+  constructor(
+    rank = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+    suit = ["clubs", "diamonds", "hearts", "spades"]
+  ) {
+    super(rank, suit);
+    this.deck = [];
+  }
+
+  // // to generate a fresh deck of cards
+  generateCards() {
+    this.deck = [];
+    this.rank.forEach((value) => {
+      this.suit.forEach((element) => {
+        this.deck.push({ rank: value, suit: element });
+      });
+    });
+  }
+
+  // use the modified Fisher-Yates shuffle algorithm
+  shuffle() {
+    for (let i = this.deck.length - 1; i >= 0; i--) {
+      const randomCardIdx = Math.round(Math.random() * this.deck.length);
+      [this.deck[i], this.deck[randomCardIdx]] = [
+        this.deck[randomCardIdx],
+        this.deck[i],
+      ];
+    }
   }
 }
 
 class Players {
   constructor(name, wealth) {
     this.name = name;
-    this.wealth = wealth;
     this.hand = [];
+    this.wealth = wealth;
   }
 }
 
-function generateCards() {}
+/*-------------------------------- Constants --------------------------------*/
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+const deck1 = new Deck();
+deck1.generateCards();
+deck1.shuffle();
+console.log(deck1.deck);
