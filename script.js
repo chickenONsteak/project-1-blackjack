083de0b22b9001eaa958da0edf5ace10f32b19db
chickenONsteak <austinchenwd@gmail.com>
2025-07-20 18:09:27 +0800

    FE

--- script.js
+++ script.js
@@ -29,7 +29,11 @@ class Deck extends Cards {
     this.deck = [];
     this.rank.forEach((value) => {
       this.suit.forEach((element) => {
-        this.deck.push({ rank: value, suit: element });
+        this.deck.push({
+          rank: value,
+          suit: element,
+          imageURI: element + value.toString(),
+        });
       });
     });
   }
@@ -76,7 +80,7 @@ class Players {
   // player places bet
   bet(amount) {
     // check if sufficient balance
-    if (amount > this.balance) {
+    if (amount > this.balance || amount < 100 || amount === "") {
       return -1; // invalid move
     } else {
       this.balance -= amount;
@@ -85,12 +89,20 @@ class Players {
   }
 
   // player draws a card
-  hit(hand, cardDeck) {
+  hit(hand, cardDeck, handHtmlId) {
     // check if player has split if trying to hit hand2
-    // note: without splitting, hand2 = undefined is falsy
+    // note: without splitting, hand2 = undefined is falsy while empty array is truthy
     if (hand) {
       const drawnCard = cardDeck.shift();
       hand.push(drawnCard);
+
+      // update cards displayed on hand
+      const playerHandUI = document.querySelector(`#${handHtmlId}`);
+      const img = document.createElement("img");
+      img.src = drawnCard.deck.imageURI;
+      img.alt = "front of card"; // FIND A WAY TO IMPROVE THIS
+      img.height = 176; // 11rem to px
+      playerHandUI.appendChild(img);
     } else {
       return -1;
     }
@@ -121,8 +133,43 @@ class Players {
   }
 }
 
+class Dealer {
+  constructor(name) {
+    this.name = name;
+    this.hand1 = [];
+  }
+
+  checkHandValue(hand) {
+    let totalValue;
+    const numOfAces = 0;
+    hand.forEach((card) => {
+      totalValue += card.checkCardValue();
+      if (card.rank === "A") {
+        numOfAces++;
+      }
+    });
+    // adjust value of ace to 1 if total value > 21
+    while (totalValue > 21 && numOfAces > 0) {
+      totalValue -= 10; // value of ace drops from 11 to 1
+      numOfAces--;
+    }
+    return totalValue;
+  }
+
+  hit(hand, cardDeck) {
+    const drawnCard = cardDeck.shift();
+    hand.push(drawnCard);
+  }
+}
+
 /*-------------------------------- Constants --------------------------------*/
-const yen = new Players("Yen", 1000);
+const players = [
+  new Players("Yen", 1000),
+  new Players("Brack", 1000),
+  new Players("Jack", 1000),
+  new Dealer("Dealer"),
+];
+const [yen, jack, brack, dealer] = players; // destructure to create 3 variables in 1 go
 
 /*---------------------------- Variables (state) ----------------------------*/
 
@@ -130,11 +177,15 @@ const yen = new Players("Yen", 1000);
 const landingPage = document.querySelector("#landing-page");
 const startButton = document.querySelector("#start-game");
 const gamePage = document.querySelector("#game-page");
+const messageBoard = document.querySelector("#message-board");
+const placeBetUI = document.querySelector("#place-bets");
 const betInput = document.querySelector("#amount");
 const quickBet1 = document.querySelector("#quick1");
 const quickBet2 = document.querySelector("#quick2");
 const quickBet3 = document.querySelector("#quick3");
 const placeBetButton = document.querySelector("#bet");
+const postBetUI = document.querySelector("#post-bets");
+const amount1Display = document.querySelector("#amount1");
 /*-------------------------------- Functions --------------------------------*/
 function populateBet(event) {
   const amountStr = event.target.innerText.replace(",", "");
@@ -145,8 +196,37 @@ function populateBet(event) {
 
 function placeBet() {
   const amount = betInput.value;
-  yen.bet(amount);
+  if (yen.bet(amount) !== -1) {
+    // acknowledge and show next phase UI
+    messageBoard.innerText = `You've bet $${amount} on your hand, game on!`;
+    placeBetUI.style.display = "none";
+    postBetUI.style.display = "grid";
+
+    // populate bet1 for hand1
+    amount1Display.innerText = amount;
+  } else {
+    messageBoard.innerText = `You can only bet an amount >$100 and less than your balance: $${yen.balance}.`;
+  }
 }
+
+function prepareDeck() {
+  const deck = new Deck();
+  deck.generateCards();
+  deck.shuffle();
+  return deck;
+}
+
+function distributeCards() {
+  const shuffledDeck = prepareDeck();
+  // each player draw 2 cards each facing up
+  for (let orbits = 0; orbits < 2; orbits++) {
+    for (const player of players) {
+      player.hit(player.hand1, shuffledDeck);
+    }
+  }
+}
+
+// INCLUDE COMPUTER LOGIC FOR CHECKING VALUE AND DETERMINE STAND/BUST AFTER HITTING
 
 /*----------------------------- Event Listeners -----------------------------*/
 // LANDING PAGE
@@ -159,4 +239,7 @@ startButton.addEventListener("click", () => {
 quickBet1.addEventListener("click", populateBet);
 quickBet2.addEventListener("click", populateBet);
 quickBet3.addEventListener("click", populateBet);
-placeBetButton.addEventListener("click", () => {});
+placeBetButton.addEventListener("click", placeBet);
+
+/*------------------------------- Game Logic --------------------------------*/
+// while (yen.balance > 0) {}
